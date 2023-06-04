442b6982e1304ecc91f0754864f473fe6ad9aaca
patriklussi <patrik.lussi@gmail.com>
2023-06-04 14:05:53 +0200

    fixes etc

---
+++ components/Navbar.tsx
@@ -0,0 +1,8 @@
+
+const Navbar = () => {
+  return (
+    <div>This is the nav</div>
+  )
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        neutral: "#F6F1F1",
+        mutedBlue: "#AFD3E2",
+        activeBlue: "#19A7CE",
+        deadBlue: "#146C94",
       },
     },
   },
   plugins: [],
-}
+};
